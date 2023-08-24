d054f440b8cf43dd599b8593c9808a436de1b792
ila36IX <jbariali002@gmail.com>
2023-08-24 01:20:30 +0000

    Add main.h header

---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+#include <stdarg.h>
+int got_percent(va_list arg, char c);
+int _putchar(char c);
+void _puts(char *str);
+int _printf(const char *format, ...);
+int _strlen(const char *s);
+
+#endif /* _MAIN_H */
